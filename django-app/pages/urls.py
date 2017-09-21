55a2f7daf9c9e85fb560244fa675873667d44d2d
Scott Greenup <scott.j.greenup@gmail.com>
2017-09-22 01:03:21 +1000

    add about page

--- django-app/pages/urls.py
+++ django-app/pages/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
 ]
