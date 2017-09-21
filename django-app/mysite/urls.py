5b29b3f9c2c2cef1f5ce9d7439e319afa61384ab
Scott Greenup <scott.j.greenup@gmail.com>
2017-09-22 00:50:05 +1000

    add index.html from v1 of site

--- django-app/mysite/urls.py
+++ django-app/mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('pages.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
 ]
---
+++ django-app/pages/views.py
@@ -0,0 +1,6 @@
+from django.views import View
+
+
+class IndexView(View):
+    def get(self, request):
+        return None
