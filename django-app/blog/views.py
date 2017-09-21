253cdf30df99f11d35cef7026e331943efcdd281
Scott Greenup <scott.j.greenup@gmail.com>
2017-09-21 23:40:55 +1000

    add view for posts and post

--- django-app/blog/views.py
+++ django-app/blog/views.py
@@ -1,12 +1,25 @@
-from django.http import HttpResponse
+from blog.models import Post
+from django.shortcuts import render
 from django.views import View
 
 
 class PostsView(View):
     def get(self, request):
-        return HttpResponse("Hi")
+        try:
+            posts = Post.objects.all().order_by("-created_at")
+        except:
+            posts = None
+
+        context = {'posts': posts}
+        return render(request, 'blog/posts.html', context)
 
 
 class PostView(View):
     def get(self, request, title):
-        return HttpResponse("title: {}".format(title))
+        try:
+            post = Post.objects.get(title=title)
+        except Post.DoesNotExist:
+            post = None
+
+        context = {'post': post}
+        return render(request, 'blog/post.html', context)
